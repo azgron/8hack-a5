a0e54b09c1740bf27ebcacb93bf61ee3c59b277c
Snir Broshi <snir@broshi.net>
2017-06-19 22:05:36 +0300

    initial graph components with chart.js

---
+++ src/reportChart/reportChart.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Chart from 'chart.js';
+
+class ReportChart extends Component {
+  componentDidMount() {
+    const ctx = this.refs['canvas'].getContext('2d');
+    this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(153, 102, 255)',
+                    'rgb(255, 159, 64)',
+                ],
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+  }
+  render() {
+    return (
+        <canvas ref="canvas"></canvas>
+    );
+  }
+}
+
+export default ReportChart;
